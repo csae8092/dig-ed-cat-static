4e9a903ec81cf4888fd5e3d65e2c1a2a2befa60f
csae8092 <peter.andorfer@oeaw.ac.at>
2022-07-15 13:42:22 +0200

    should close #1

--- build.py
+++ build.py
@@ -2,30 +2,65 @@ import os
 import glob
 import jinja2
 import pandas as pd
-from config import EDITIONS
 from slugify import slugify
-print('hallo')
-os.makedirs('./html', exist_ok=True)
-for x in glob.glob('./html/*.html'):
-    os.unlink(x)
+
+from config import (
+    EDITIONS,
+    TS_CLIENT,
+    TS_SCHEMA_NAME,
+    MANDATORY_FIELDS,
+    FACET_FIELDS
+)
+
+from utils import make_schema, delete_and_create_schema, create_ts_documents
 
 templateLoader = jinja2.FileSystemLoader(searchpath=".")
 templateEnv = jinja2.Environment(loader=templateLoader)
 
+
+print('hallo, lets start building')
+
+os.makedirs('./html', exist_ok=True)
+for x in glob.glob('./html/*.html'):
+    os.unlink(x)
+    # print(f'removing {x}')
+
+
 files = glob.glob('./templates/static/*.html')
 
+print('building static content')
 for x in files:
     template = templateEnv.get_template(x)
     _, tail = os.path.split(x)
+    # print(f'rendering {tail}')
     with open(f'./html/{tail}', 'w') as f:
         f.write(template.render({"objects": {}}))
 
+print(f"fetching {EDITIONS}")
 df = pd.read_csv(EDITIONS).head(-1)
 df = df.astype('str')
 df = df.replace(['nan', ''], 'not provided')
 objects = df.to_dict(orient='records')
 labels = {slugify(x): x for x in df.keys()}
 df.columns = labels.keys()
+
+print('Typesense Index')
+print(f'defining Typesense collection schema with name: {TS_SCHEMA_NAME}')
+ts_schema = make_schema(df, TS_SCHEMA_NAME, MANDATORY_FIELDS, FACET_FIELDS)
+
+print(f'creating Typesnses Collection with schema {TS_SCHEMA_NAME}')
+cur_schema = delete_and_create_schema(
+    TS_CLIENT, TS_SCHEMA_NAME, ts_schema
+)
+print(f"schema created at {cur_schema['created_at']}")
+print("create documents to index")
+documents = create_ts_documents(df, FACET_FIELDS)
+print(f"created {len(documents)} documents to index")
+
+index = TS_CLIENT.collections[TS_SCHEMA_NAME].documents.import_(documents, {'action': 'create'})
+print(f"status of index-process: {index[0]}")
+
+
 template = templateEnv.get_template('./templates/edition.html')
 for i, object in enumerate(df.to_dict(orient='records')):
     f_name = f"entry-{i+1:003}.html"
@@ -37,6 +72,7 @@ for i, object in enumerate(df.to_dict(orient='records')):
         for key, value in object.items()
     }
     with open(f"html/{f_name}", 'w') as f:
+        # print(f"rendering {f_name}")
         f.write(
             template.render(
                 {
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 """ some helper functions """
+import typesense
 
 
 def make_schema(df, ts_schema_name, mandatory_fields, facet_fields):
@@ -33,3 +34,27 @@ def make_schema(df, ts_schema_name, mandatory_fields, facet_fields):
                 }
             )
     return TS_SCHEMA
+
+
+def delete_and_create_schema(ts_client, ts_schema_name, ts_schema):
+    try:
+        ts_client.collections[ts_schema_name].delete()
+    except typesense.api_call.ObjectNotFound:
+        pass
+    created = ts_client.collections.create(ts_schema)
+    return created
+
+
+def create_ts_documents(df, facet_fields):
+    documents = []
+    for i, row in df.iterrows():
+        doc = {
+            'id': f"{i+1}"
+        }
+        for key in df.keys():
+            if key in facet_fields:
+                doc[key] = [x.strip() for x in row[key].split(';')]
+            else:
+                doc[key] = row[key]
+        documents.append(doc)
+    return documents
